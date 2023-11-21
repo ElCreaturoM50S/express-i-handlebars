87852c44787e78020b5dd7d1c1f2717221c01efa
bobu <113674920+ElCreaturoM50S@users.noreply.github.com>
2023-11-21 15:56:45 +0100

    dionzury

--- index.js
+++ index.js
@@ -1,49 +1,50 @@
-const express = require("express")
-const expressHB = require("express-handlebars")
-const path = require('path');
+const express = require("express");
+const expresshandler = require("express-handlebars");
+
+const makaronDane = require("./public/data/makaronyDane.json");
 
 const app = express()
 
-app.engine('handlebars', expressHB.engine({extname: 'handlebars', defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+app.engine("handlebars", expresshandler.engine());
+app.set("view engine", "handlebars")
+app.set("views", "./views")
 
 app.use(express.static("public"))
 
-app.get("/",(req,res)=>{
-    res.render("home", { title: "Portal o makaronach", content: "to jest tresc"})
-})
-
-app.get('/pasta/:id', (req,res) => {
-    const pastaId = req.params.id;
-    const pastaData = [
-        {},
-        {
-          name: 'Spaghetti Carbonara',
-          description: 'Spaghetti Carbonara to popularne włoskie danie przygotowywane z makaronu spaghetti, jajek, sera parmezan, boczku i pieprzu. Jest to proste, ale smaczne danie, które pochodzi z Rzymu.'
-        },
-        {
-          name: 'Penne all\'Arrabbiata',
-          description: 'Penne all\'Arrabbiata to włoskie danie z rurkowatym makaronem penne w sosie Arrabbiata, który jest pikantny i przygotowywany z pomidorów, czosnku, oliwy z oliwek i papryczek chili.'
-        },
-        {
-          name: 'Fettuccine Alfredo',
-          description: 'Fettuccine Alfredo to danie pochodzące z włoskiej kuchni, składające się z szerokich wstążek makaronu fettuccine w sosie z masła, śmietanki i sera parmezan.'
-        },
-        {
-          name: 'Linguine with Clam Sauce',
-          description: 'Linguine with Clam Sauce to włoskie danie z makaronem linguine, które jest serwowane z sosem na bazie małży, czosnku, oliwy z oliwek i białego wina.'
-        },
-        {
-          name: 'Ravioli',
-          description: 'Ravioli to rodzaj włoskiego makaronu, który jest wypełniony różnymi nadzieniami, takimi jak mięso, ser, szpinak lub grzyby. Jest często serwowany z sosem pomidorowym lub masłem i szałwią.'
-        },
-        {
-            name: 'Spaghetti Carbonara',
-            description: 'Spaghetti Carbonara to popularne włoskie danie przygotowywane z makaronu spaghetti, jajek, sera parmezan, boczku i pieprzu. Jest to proste, ale smaczne danie, które pochodzi z Rzymu.'
-        },
-      ];
-    res.render('pasta', {title: 'Makaron', pasta: pastaData[pastaId]});
-    console.log(pastaData[pastaId])
+app.get("/", (req,res) => {
+  res.render("home", {
+    makaronDane: makaronDane,
+  });
 });
 
-app.listen(8080, ()=>console.log("server działa"))
+app.get("/pasta/:id", (req,res) => {
+  //:id daje nam (id) fragment url po :
+  const { id } = req.params;
+  //makaron z makaronDane[id]
+  const makaron = makaronDane[id]
+
+  //jezeli mamy makaron z makaron id to jest git
+  if (makaron) {
+    //render pasta view with makaronDane
+    res.render("pasta", {
+      makaronDane: makaron,
+    });
+  } else {
+    //handle error 404
+    //if respond is 404(page not found then)
+    res.status(404).render("404")
+  }
+});
+
+
+//* jest kazdym innym url ktory nie jest w innych getach
+app.get("*", (req, res) => {
+  //tstatus zwraca status
+  res.status(404).render("404")
+});
+
+const port = 2137 
+
+app.listen(port, () => {
+  console.log(`Czy to jest serwer ${port}?, tak to jest serwer ${port}`)
+})
